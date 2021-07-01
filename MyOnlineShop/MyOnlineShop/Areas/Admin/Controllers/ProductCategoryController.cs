7c204c14399cdcfc526736c641311e15d42b3cce
FIROZ <firozCSE38@gmail.com>
2021-07-01 14:19:31 -0400

    Initial Commit

---
+++ MyOnlineShop/MyOnlineShop/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyOnlineShop.Data;
+using MyOnlineShop.Models;
+
+namespace MyOnlineShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductCategoryController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public ProductCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            // var data = _db.ProductTypes.ToList();
+            return View(_db.ProductTypes.ToList());
+        }
+
+        //Create Get Action Method
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Create Post Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ProductTypes.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+        }
+    }
+ }
+
+
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using MyOnlineShop.Data;
+//using MyOnlineShop.Models;
+
+//namespace MyOnlineShop.Areas.Admin.Controllers
+//{
+   
+//    public class ProductTypesController : Controller
+//    {
+
+
+        
+
+//    }
+//}
---
+++ MyOnlineShop/MyOnlineShop/Models/ProductTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyOnlineShop.Models
+{
+    public class ProductTypes
+    {
+        public int Id { set; get; } 
+        [Required]
+        [Display (Name="Product Type")]
+        public string ProductType { set; get; }  
+    }
+}
